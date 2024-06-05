73a4e2806f2b196f869e77994f977f0f7bd8ba83
rgarcia <rgarcia@106-19.info2>
2024-06-05 08:23:11 +0200

    Ejercicio 2 terminado

--- src/ejercicio2/Oficial.java
+++ src/ejercicio2/Oficial.java
@@ -1,7 +1,20 @@
 package ejercicio2;
 
-public class Oficial extends Empleado{
+public class Oficial extends Operario{
 
-	
+	public Oficial(String nombre) {
+		super(nombre);
+	}
+
+	@Override
+	public String toString() {
+		
+		String cad = super.toString();
+		
+		cad += " -> Oficial";
+		
+		return cad;
+		
+	}
 	
 }
--- src/ejercicio2/Tecnico.java
+++ src/ejercicio2/Tecnico.java
@@ -1,5 +1,19 @@
 package ejercicio2;
 
-public class Tecnico {
+public class Tecnico extends Operario{
 
+	public Tecnico(String nombre) {
+		super(nombre);
+	}
+	
+	@Override
+	public String toString() {
+		
+		String cad = super.toString();
+		
+		cad += " -> Tecnico";
+		
+		return cad;
+	}
+	
 }
